75378ec9d92abb62427e157b570299a7053e690f
unknown <niccolo.panciroli@gmail.com>
2021-02-11 07:42:43 +0100

    uploaded file eseercizio_numero28_pagina190.py

--- esercizio_numero28_pagina190.py
+++ esercizio_numero28_pagina190.py
@@ -1,16 +1,34 @@
 '''
-Organizza con un dizionario la rubrica con i nomi delle persone e i rispettivi numeri telefonici. Fornito poi il nome della persona,
-il programma visualizza il suo numero di telefono oppure un messaggio nel caso la persona non sia presente nella rubrica.
+I nomi delle città e i corrispondenti Codici di Avviamento Postali (CAP), vengono inseriti da tastiera e memorizzati in un dizionario,
+dove il CAP è la chiave. Fornito poi il nome di una città, costruisci un messaggio che visualizzi il suo CAP oppure un messaggio nel caso la
+città non sia compresa nell'elenco. Analagomente, fornendo il CAP restituisce il nome della città oppure un messaggio di errore.
 '''
-pagine_bianche = {"Mario":123,
-                "Luigi":456,
-                "Lorenzo":789,
-                "Giovanni":987,
-                "Giulio":654,
-                "Enrico":321}
-nome = input("Inserisci il nome della persona di cui cercare il numero di telefono nelle rubrica ")
+cap_città = {}
+città_cap = {}
 
-if nome in pagine_bianche:
-    print("Il numero di telefono del seguente dell'utente ", nome," è ", pagine_bianche[nome])
+while True:
+    print("Per ogni città dovrai inserire un CAP, se vuoi concludere la compilazione del dato scrivi STOP ")
+    input()
+    città = input("Inserisci il nome della città ")
+    if città == "STOP":
+        print("bella")
+        break
+    else:
+        cap = input("Inserisci il cap della città ")
+        cap_città[cap]= città
+        città_cap[città] = cap
+print("Il resoconto finale è ",cap_città)
+print(città_cap)
+risposta = input("Fai una scelta (DIGITA 1 o 2) \n1) Vuoi sapere il CAP, conoscendo la città \n2) Vuoi sapere la città, conoscendo il CAP \n")
+if risposta == "1":
+    nome_città = input("Inserisci il nome della città della quale vuoi sapere il CAP ")
+    if nome_città in cap_città.values():
+        print("Il CAP associato alla città ", nome_città, " è ", città_cap[nome_città])
+    else:
+        print("Non è presente alcun CAP associato alla città ", nome_città)
 else:
-    print("Il numero di telefono dell'utente richiesto non è presente nella rubrica")
+    nome_cap = input("Inserisci il cap della città della quale vuoi sapere il nome ")
+    if nome_cap in cap_città:
+        print("La città associata al CAP ", nome_cap, " è ", cap_città[nome_cap])
+    else:
+        print("Non è presente nessuna città associata al CAP ", nome_cap)
